a1b9981529b95dce02fa5274efc7a76c27ec40ac
AmesiJustice <justiceamesi6@gmail.com>
2023-06-15 21:27:19 -0700

    4.Puts : escaped the opening quotation

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char Mystr[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s\n", Mystr);
+
+	return (0);
+}
