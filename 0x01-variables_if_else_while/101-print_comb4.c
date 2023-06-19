0dd40d6e4f2d35a9d89153d5cae2a86d222fd0c0
AmesiJustice <justiceamesi6@gmail.com>
2023-06-19 10:40:05 -0700

    11. The success combination in business is: Do what you do better... and: do more of what you do... fixed betty trailing whitespaces

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,37 +7,37 @@
  */
 int main(void)
 {
-        int combo1 = 0;
-        int combo2 = 1;
+	int combo1 = 0;
+	int combo2 = 1;
 	int combo3;
-        int count = 0;
+	int count = 0;
 
-        while (combo1 <= 8)
-        {
-                combo2 = combo1 + 1;
+	while (combo1 <= 8)
+	{
+		combo2 = combo1 + 1;
 
-                while (combo2 <= 9)
-                {
+		while (combo2 <= 9)
+		{
 			combo3 = combo2 + 1;
 
 			while (combo3 <= 9)
-                     {
-			     putchar(combo1 + '0');
-			     putchar(combo2 + '0');
-			     putchar(combo3 + '0');
+			{
+				putchar(combo1 + '0');
+				putchar(combo2 + '0');
+				putchar(combo3 + '0');
 
-			     if (count != 125)
-                        {
-                                putchar(',');
-                                putchar(' ');
-                        }
-                        combo3++;
-                        count++;
-		     }
-                combo2++;
+				if (count != 119)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				combo3++;
+				count++;
+			}
+			combo2++;
 		}
 		combo1++;
 	}
-        putchar('\n');
-        return (0);
+	putchar('\n');
+	return (0);
 }
