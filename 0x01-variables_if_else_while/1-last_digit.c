6040358646428359c112f73471592e51e26c0d3e
AmesiJustice <justiceamesi6@gmail.com>
2023-06-18 21:25:43 -0700

    1. The last digit : removed variable lastDigitand nested n % 10 into the if statement

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,22 +9,19 @@
  */
 int main(void)
 {
-	int n, lastDigit;
+	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	lastDigit = n % 10;
-
-	printf("Last digit of %d is %d", n, lastDigit);
-	if (n > 5)
-		printf(" and is greater than 5\n");
-	else if (n == 0)
+	if (n % 10 == 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
+	else if (n % 10 == 0)
 	{
-		printf(" and is 0\n");
+		printf("Last digit of %d is %d and is 0\n", n, n % 10);
 	}
-	else if (n < 6 && !0)
+	else if (n % 10 < 6 && !0)
 	{
-		printf(" and is less than 6 and not 0\n");
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 	}
 	return (0);
 }
