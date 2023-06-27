8c77de9f57443445ccbe8a94ea90a19a4179e7f0
AmesiJustice <justiceamesi6@gmail.com>
2023-06-27 16:43:50 -0700

    COMMIT

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Print the alphabet 10 times
+ */
+void print_alphabet_x10(void)
+{
+	char alpha = 'a';
+
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		while (alpha < 'z')
+		{	
+			_putchar(alpha);
+			_putchar('\n');
+			alpha++;
+		}
+	}
+}
