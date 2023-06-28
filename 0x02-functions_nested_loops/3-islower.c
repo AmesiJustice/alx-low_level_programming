cd8e70790f84531e5a464dc57cc582adb06d9e77
AmesiJustice <justiceamesi6@gmail.com>
2023-06-27 17:11:54 -0700

    COMMIT

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * int _islower - Checks for lowercase character
+ */
+int _islower(int c)
+{
+	if (islower != 0)
+		return (1);
+	else
+		return (0);
+
+}
