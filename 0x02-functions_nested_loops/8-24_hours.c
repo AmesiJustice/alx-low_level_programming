9285a69c85b97c476dcd9b5af3f2303a879106ce
AmesiJustice <justiceamesi6@gmail.com>
2023-06-27 18:37:02 -0700

    COMMIT

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * jack_bauer -prints every minute of the day starting from 00:00 to 23:59
+ */
+void jack_bauer(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a <= 23; a++)
+	{
+		for (b = 0; b <= 59; b++)
+		{
+		printf("%02d:%02d", a, b);
+		}
+		printf("\n");
+	}
+}
