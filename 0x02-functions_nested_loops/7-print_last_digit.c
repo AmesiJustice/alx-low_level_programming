bea36b6c5a903088fbe201668be21582f77a8f54
AmesiJustice <justiceamesi6@gmail.com>
2023-06-27 18:19:14 -0700

    COMMIT

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,5 +7,14 @@
  */
 int print_last_digit(int n)
 {
-	return (n % 10);
+	if (n >= 0)
+	{
+		_putchar('0' + (n % 10));
+		return ((n % 10));
+	}
+	else
+	{
+		_putchar('0' + (-(n % 10)));
+		return (-(n % 10));
+	}
 }
